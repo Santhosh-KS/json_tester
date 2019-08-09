e2a6768d2bf9b0efb36901fdb68dd710cfeb7c28
KS <Santhosh.KS@partner.bmw.de>
2019-08-09 10:14:34 +0200

    Accessing nested json

--- jsonparser/generic_json_parser.cpp
+++ jsonparser/generic_json_parser.cpp
@@ -4,6 +4,9 @@
 
 #include "generic_json_parser.hpp"
 
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
 bool GenericJsonParser::FileExists(const std::string &file) const
 {
     std::ifstream f(file.c_str());
@@ -12,7 +15,7 @@ bool GenericJsonParser::FileExists(const std::string &file) const
 
 GenericJsonParser::GenericJsonParser(const std::string &file)
     : is_file_exists_(FileExists(file)),
-      file_name_(file)
+    file_name_(file)
 {
 }
 
@@ -37,7 +40,7 @@ bool GenericJsonParser::Parse()
 bool GenericJsonParser::ValidateJson(const std::string &json_data) 
 {
     document_.Parse(json_data.c_str());
-    
+
     return document_.IsObject();
 }
 
@@ -48,25 +51,67 @@ bool GenericJsonParser::HasMember(const std::string &key) const
 
 bool GenericJsonParser::IsArray(const std::string &key) const
 {
-    return document_.IsArray();
+    return document_[key.c_str()].IsArray();
 }
 
 bool GenericJsonParser::IsNumber(const std::string &key) const
 {
-    return document_.IsNumber();
+    return document_[key.c_str()].IsNumber();
 }
 
 bool GenericJsonParser::IsDouble(const std::string &key) const
 {
-    return document_.IsDouble();
+    return document_[key.c_str()].IsDouble();
 }
 
 bool GenericJsonParser::IsBool(const std::string &key) const
 {
-    return document_.IsBool();
+    return document_[key.c_str()].IsBool();
 }
 
 bool GenericJsonParser::IsString(const std::string &key) const
 {
-    return document_.IsString();
+    return document_[key.c_str()].IsString();
 }
+
+std::uint32_t GenericJsonParser::ArraySize(const std::string &key) const
+{
+    std::uint32_t retVal{0};
+    if (IsArray(key))
+    {
+        retVal = static_cast<std::uint32_t>(document_[key.c_str()].Capacity());
+    }
+    return retVal;
+}
+
+bool GenericJsonParser::ArrayElements(const std::string &key) const
+{
+    const rapidjson::Value& val = document_[key.c_str()];
+    for (auto &v : val.GetArray())
+    {
+        const auto &lanes_boundaries{v["lanes_boundaries"] };
+        // std::cout << "lanes_boundaries type " << (lanes_boundaries.GetType()) << "\n";
+        for(size_t i = 0 ; i < lanes_boundaries.Size(); i++)
+        {
+            std::cout << " ################# \n";
+            std::cout << "my lanes_boundaries = " << i << "\n";
+            const auto &poly_line = lanes_boundaries[i]["polyline"];
+            std::cout << "my polyline type " << (poly_line.GetType()) << "\n";
+            std::cout << " ################# \n";
+            for(size_t j = 0 ; j < poly_line.Size(); j++)
+            {
+                //std::cout << "my polyline " << (poly_line[j].HasMember("point_end")) << "\n";
+                std::cout << " ################# \n";
+                std::cout << "my poly_line = " << j << "\n";
+
+                const auto &points = poly_line[j]["point_end"];
+                std::cout << "my points type " << (points.GetType()) << "\n";
+                std::cout << "my point x = " << (points["x"].GetDouble()) << "\n";
+                std::cout << "my point y = " << (points["y"].GetDouble()) << "\n";
+                std::cout << " ################# \n";
+            }
+        }
+    }
+    return true;
+}
+
